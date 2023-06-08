f94a7dc28e26bf4a087e94c2d34ab6ba2805ca6f
SofiiaBesarab <sofiia.besarab31@gmail.com>
2023-06-07 17:29:30 -0700

    Made a loop with forecast days

--- src/index.js
+++ src/index.js
@@ -16,6 +16,34 @@ if (minutes < 10) {
 h4.innerHTML = `${day}, ${hours}:${minutes}`;
 
 //2
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+            />
+            <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   celsiusTemperature = Math.round(response.data.main.temp);
   document.querySelector("#result").innerHTML = response.data.name;
@@ -70,3 +98,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displayForecast();
